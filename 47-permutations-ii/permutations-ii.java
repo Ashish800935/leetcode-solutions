6a10c9c48c03fe77a397abf848529ecdf3d1a089
Ashish800935 <ashishkumarkushwaha716@gmail.com>
2026-08-02 14:37:38 +0530

    Time: 3 ms (91.32%) | Memory: 46.7 MB (44.39%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -2,28 +2,26 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        helper(nums, res, ans, map);
+        Arrays.sort(nums);
+        boolean[]used=new boolean[nums.length];
+        helper(nums, res, ans, used);
         return res;
     }
 
-    void helper(int[] nums, List<List<Integer>> res, List<Integer> ans, HashMap<Integer, Integer> map) {
+    void helper(int[] nums, List<List<Integer>> res, List<Integer> ans,boolean[]used) {
         if (ans.size() == nums.length) {
             res.add(new ArrayList<>(ans));
             return;
         }
-       for (Integer key : map.keySet()){
-        if(map.get(key)==0){
-            continue;
-        }
-        map.put(key, map.get(key) - 1);
-        ans.add(key);
-        helper(nums,res,ans,map);
+       for(int i=0;i<nums.length;i++){
+        if(used[i]) continue;
+        if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+
+        used[i]=true;
+        ans.add(nums[i]);
+        helper(nums,res,ans,used);
         ans.remove(ans.size()-1);
-        map.put(key,map.get(key)+1);
+        used[i]=false;
        }
 
     }
